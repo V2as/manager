bcbecd306db3d5e71dcff5e878ccfbf14562fe97
V2as <115551035+V2as@users.noreply.github.com>
2024-10-18 12:25:23 +0300

    Update init_server.sh
    поправилл

--- init_server.sh
+++ init_server.sh
@@ -5,20 +5,20 @@ apt install build-essential curl git wget -y
 apt update
 
 mkdir -p /opt/go
-cd /opt/go
-wget https://go.dev/dl/go1.22.0.linux-amd64.tar.gz
+
+wget https://go.dev/dl/go1.22.0.linux-amd64.tar.gz > /opt/go
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.22.0.linux-amd64.tar.gz
 echo "export PATH=$PATH:/usr/local/go/bin" >> /etc/profile
 source /etc/profile
 
 git clone https://github.com/amnezia-vpn/amneziawg-go.git /opt/amnezia-go
-cd /opt/amnezia-go
-make
+/opt/amnezia-go/make
+
 cp /opt/amnezia-go/amneziawg-go /usr/bin/amneziawg-go
 
 git clone https://github.com/amnezia-vpn/amneziawg-tools.git /opt/amnezia-tools
-cd /opt/amnezia-tools/src
-make
-make install
+/opt/amnezia-tools/src/make
+/opt/amnezia-tools/src/make install
+
 
 
